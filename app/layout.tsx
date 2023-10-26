3e1afb2255c0f1514fe0f9a186a4744fbbb9974a
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2023-10-26 19:53:16 -0300

    nexth auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Link from 'next/link'
+import Navbar from '@/components/Navbar'
+import Providers from './Providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,57 +19,10 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <div className='navbar bg-base-100'>
-          <div className='navbar-start w-auto'>
-            <div className='dropdown'>
-              <label tabIndex={0} className='btn btn-ghost lg:hidden'>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  className='h-5 w-5'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  stroke='currentColor'>
-                  <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    strokeWidth='2'
-                    d='M4 6h16M4 12h8m-8 6h16'
-                  />
-                </svg>
-              </label>
-              <ul
-                tabIndex={0}
-                className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52'>
-                <li>
-                  <Link href={'/objetivos'}>Objetivos</Link>
-                </li>
-                <li>
-                  <Link href={'/entregas'}>Entregas</Link>
-                </li>
-                <li>
-                  <Link href={'/seguimiento'}>Seguimiento</Link>
-                </li>
-              </ul>
-            </div>
-            <Link href={'/'} className='btn btn-ghost normal-case text-xl'>
-              LeanSys
-            </Link>
-          </div>
-          <div className='navbar-center hidden lg:flex mx-auto'>
-            <ul className='menu menu-horizontal px-1'>
-              <li>
-                <Link href={'/objetivos'}>Objetivos</Link>
-              </li>
-              <li>
-                <Link href={'/entregas'}>Entregas</Link>
-              </li>
-              <li>
-                <Link href={'/seguimiento'}>Seguimiento</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-        {children}
+        <Providers>
+          <Navbar />
+          {children}
+        </Providers>
       </body>
     </html>
   )
