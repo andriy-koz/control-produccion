e8903fdc1171d550f271da9e4a9cfcddd40525be
andriyskb <148741323+andriyskb@users.noreply.github.com>
2023-10-26 18:48:50 +0000

    TODO re renderizar al enviar entrega

--- app/entregas/page.tsx
+++ app/entregas/page.tsx
@@ -1,124 +1,106 @@
 'use client'
 
-import { useState, useEffect } from 'react'
-import { collection, getDocs, query, doc, setDoc } from 'firebase/firestore'
-import { database } from '../../firebase'
+import { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
+import { doc, setDoc } from 'firebase/firestore'
+import { database } from '@/firebase'
+import useGetData from '@/hooks/useGetData'
+import { sumarEntregas } from '@/utlis/sumarEntregas'
 
 export default function Entregas() {
-  // Obtener todos los objetivos
-  const [objetivos, setObjetivos] = useState<any>([])
-  const [entregas, setEntregas] = useState<any>([])
+    const [entregas, objetivos, horaActual, minActual] = useGetData()
+    const [estadoEntregas, setEstadoEntregas] = useState()
 
-  useEffect(() => {
-    const getEntregas = async () => {
-      const q = query(collection(database, 'entregas'))
-      const querySnapshot = await getDocs(q)
-      const tempEntregas: any = []
-      querySnapshot.forEach((doc: any) => {
-        tempEntregas.push(doc.data())
-      })
-      setEntregas(tempEntregas)
-    }
-    getEntregas()
-    const getObjetivos = async () => {
-      const q = query(collection(database, 'objetivos'))
-      const querySnapshot = await getDocs(q)
-      const tempObjetivos: any = []
-      querySnapshot.forEach((doc: any) => {
-        tempObjetivos.push(doc.data())
-      })
-      setObjetivos(tempObjetivos)
-    }
-    getObjetivos()
-  }, [])
+    // Manejar las entregas correspondientes a cada objetivo
+    const handleEntrega = async (
+        e: any,
+        modelo: any,
+        pieza: any,
+        id_objetivo: any,
+        total_entregas : any
+    ) => {
+        e.preventDefault()
 
-  const sumarEntregas = (modelo: string, pieza: string, id: any) => {
-    let sum = 0
-    entregas.forEach((entrega: any) => {
-      if (
-        entrega.modelo === modelo &&
-        entrega.pieza === pieza &&
-        entrega.id_objetivo === id
-      ) {
-        sum += parseInt(entrega.cantidad_entrega)
+        const date = new Date()
+
+        const mins = date.getMinutes()
+        const hs = date.getHours()
+
+        const entrega = {
+            cantidad_entrega: parseInt(e.target.cantidad_entrega.value),
+            pieza,
+            sector: 'soldadura',
+            modelo,
+            hora: hs,
+            minuto: mins,
+            id: uuidv4(),
+            id_objetivo,
+        }
+
+        const docRef = doc(database, 'entregas', entrega.id)
+        await setDoc(docRef, entrega)
+        setEstadoEntregas(total_entregas)
       }
-    })
-    return sum
-  }
-
-  // Manejar las entregas correspondientes a cada objetivo
-  const handleEntrega = async (
-    e: any,
-    modelo: any,
-    pieza: any,
-    id_objetivo: any
-  ) => {
-    e.preventDefault()
-
-    const date = new Date()
-
-    const mins = date.getMinutes()
-    const hs = date.getHours()
-
-    const entrega = {
-      cantidad_entrega: parseInt(e.target.cantidad_entrega.value),
-      pieza,
-      sector: 'soldadura',
-      modelo,
-      hora: hs,
-      minuto: mins,
-      id: uuidv4(),
-      id_objetivo,
-    }
-
-    const docRef = doc(database, 'entregas', entrega.id)
-    await setDoc(docRef, entrega)
-    setEntregas([...entregas, entrega])
-  }
-
-  return (
-    <div>
-      {objetivos &&
-        objetivos.map((objetivo: any) => {
-          const entregas = sumarEntregas(
-            objetivo.modelo,
-            objetivo.pieza,
-            objetivo.id
-          )
-          const progreso = (entregas / objetivo.cantidad_objetivo) * 100
-          return (
-            <form
-              onSubmit={e =>
-                handleEntrega(e, objetivo.modelo, objetivo.pieza, objetivo.id)
-              }
-              key={objetivo.id}
-              className='px-8 py-4 my-4 bg-base-200'>
-              <div className='flex items-center justify-center gap-6'>
-                <div className='flex-1 text-center'>
-                  <p className='uppercase text-secondary'>{objetivo.modelo}</p>
-                  <p className='uppercase'>{objetivo.pieza}</p>
-                  <input
-                    type='number'
-                    name='cantidad_entrega'
-                    defaultValue={objetivo.cantidad_entrega}
-                    className='input input-bordered input-primary w-full max-w-xs text-center'
-                  />
-                </div>
-                <button type='submit' className='btn btn-primary flex-1'>
-                  Enviar
-                </button>
-              </div>
-              <progress
-                className='progress progress-success w-full mt-4'
-                value={progreso}
-                max='100'></progress>
-              <p className='text-center text-sm'>
-                {entregas} / {objetivo.cantidad_objetivo}
-              </p>
-            </form>
-          )
-        })}
-    </div>
-  )
+      
+      return (
+        <div>
+            {objetivos &&
+                objetivos.map((objetivo: any) => {
+                  const { total_entregas } = sumarEntregas(
+                    entregas,
+                    objetivo.modelo,
+                    objetivo.pieza,
+                    objetivo.id
+                    )
+                    const progreso =
+                        (total_entregas / objetivo.cantidad_objetivo) * 100
+                    return (
+                        <form
+                            onSubmit={(e) =>
+                                handleEntrega(
+                                    e,
+                                    objetivo.modelo,
+                                    objetivo.pieza,
+                                    objetivo.id,
+                                    total_entregas
+                                )
+                            }
+                            key={objetivo.id}
+                            className="px-8 py-4 my-4 bg-base-200"
+                        >
+                            <div className="flex items-center justify-center gap-6">
+                                <div className="flex-1 text-center">
+                                    <p className="uppercase text-secondary">
+                                        {objetivo.modelo}
+                                    </p>
+                                    <p className="uppercase">
+                                        {objetivo.pieza}
+                                    </p>
+                                    <input
+                                        type="number"
+                                        name="cantidad_entrega"
+                                        defaultValue={objetivo.cantidad_entrega}
+                                        className="input input-bordered input-primary w-full max-w-xs text-center"
+                                    />
+                                </div>
+                                <button
+                                    type="submit"
+                                    className="btn btn-primary flex-1"
+                                >
+                                    Enviar
+                                </button>
+                            </div>
+                            <progress
+                                className="progress progress-success w-full mt-4"
+                                value={progreso}
+                                max="100"
+                            ></progress>
+                            <p className="text-center text-sm">
+                                {total_entregas} / {objetivo.cantidad_objetivo}
+                            </p>
+                        </form>
+                    )
+                })}
+        </div>
+    )
 }
