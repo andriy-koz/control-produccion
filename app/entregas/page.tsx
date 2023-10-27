66957b66a84b7f841bdbb7e8834e37ec259dcdd8
andriyskb <148741323+andriyskb@users.noreply.github.com>
2023-10-27 11:51:40 +0000

    fix

--- app/entregas/page.tsx
+++ app/entregas/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import { doc, setDoc } from 'firebase/firestore'
 import { database } from '@/firebase'
@@ -9,8 +8,6 @@ import { sumarEntregas } from '@/utlis/sumarEntregas'
 
 export default function Entregas() {
   const [entregas, objetivos, horaActual, minActual, refetch] = useGetData()
-  const [loading, setLoading] = useState(false)
-  const [estadoEntregas, setEstadoEntregas] = useState<any>([])
 
   const datosParaRenderizar = objetivos.map((objetivo: any) => {
     const { total_entregas } = sumarEntregas(
@@ -54,7 +51,6 @@ export default function Entregas() {
     const docRef = doc(database, 'entregas', entrega.id)
     await setDoc(docRef, entrega)
     refetch()
-    setLoading(false)
   }
 
   return (
@@ -63,37 +59,34 @@ export default function Entregas() {
         datosParaRenderizar.map((objetivo: any) => {
           return (
             <form
-              onSubmit={e => {
+              onSubmit={(e) => {
                 handleEntrega(e, objetivo.modelo, objetivo.pieza, objetivo.id)
-                setLoading(true)
               }}
               key={objetivo.id}
-              className='px-8 py-4 my-4 bg-base-200'>
-              <div className='flex items-center justify-center gap-6'>
-                <div className='flex-1 text-center'>
-                  <p className='uppercase text-secondary'>{objetivo.modelo}</p>
-                  <p className='uppercase'>{objetivo.pieza}</p>
+              className="px-8 py-4 my-4 bg-base-200"
+            >
+              <div className="flex items-center justify-center gap-6">
+                <div className="flex-1 text-center">
+                  <p className="uppercase text-secondary">{objetivo.modelo}</p>
+                  <p className="uppercase">{objetivo.pieza}</p>
                   <input
-                    type='number'
-                    name='cantidad_entrega'
+                    type="number"
+                    name="cantidad_entrega"
                     defaultValue={objetivo.cantidad_entrega}
-                    className='input input-bordered input-primary w-full max-w-xs text-center'
+                    className="input input-bordered input-primary w-full max-w-xs text-center"
                   />
                 </div>
 
-                <button type='submit' className='btn btn-primary flex-1'>
-                  {loading ? (
-                    <span className='loading loading-spinner loading-md'></span>
-                  ) : (
-                    'Entregar'
-                  )}
+                <button type="submit" className="btn btn-primary flex-1">
+                  Entregar
                 </button>
               </div>
               <progress
-                className='progress progress-success w-full mt-4'
+                className="progress progress-success w-full mt-4"
                 value={objetivo.progreso}
-                max='100'></progress>
-              <p className='text-center text-sm'>
+                max="100"
+              ></progress>
+              <p className="text-center text-sm">
                 {objetivo.total_entregas} / {objetivo.cantidad_objetivo}
               </p>
             </form>
